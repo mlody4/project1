b9537d500686178ab0d4bb638c4a707c36c35cfb
Zosia Samosia <Zosia@Samosia.pl>
2023-05-08 12:43:08 +0200

    add style optimalization

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const btn = document.querySelector('header a');
+const h1 = document.querySelector('header h1');
+const h1Text = h1.innerText;
+
+let counter = 0;
+
+btn.addEventListener('click', () => {
+    
+    counter++;
+
+    // if (counter % 2 === 0) {
+    //     h1.innerText = h1Text
+    // }else{
+    //     h1.innerText = 'Będzie Pan zadowolony :)';
+    // }
+
+    counter % 2 === 0 ? h1.innerText = h1Text : h1.innerText = 'Będzie Pan zadowolony :)';
+    // operator warunkowy, działa tak samo jak skrypt wyżej
+
+
+})
